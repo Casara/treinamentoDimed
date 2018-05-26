2bf3042f948d23bf367c8efc08f65ddd072eb2e0
MarcusFleck <mv_fleck@hotmail.com>
2018-05-26 08:40:03 -0300

    first

---
+++ treinamento-app/src/main/java/br/com/treinamento/Application.java
@@ -0,0 +1,17 @@
+package br.com.treinamento;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import br.com.dimed.arch.DimedServicesRunner;
+
+@EnableAutoConfiguration
+@EnableScheduling
+@ComponentScan(basePackages = "br.com.dimed")
+public class Application {
+
+    public static void main(String[] args) {
+        new DimedServicesRunner(Application.class).run(args);
+    }
+
+}
